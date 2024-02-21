5ab71ae34edb196759092687562dbf455609a06f
Priyansu Pattanayak <ppattanayakcodes@gmail.com>
2024-02-21 06:57:28 +0530

    made fewer changes in for easy deployement

--- Atlas/directory-2/StudyBuddy3/src/app/components/DashBoardComponents/HeadMainPart.js
+++ Atlas/directory-2/StudyBuddy3/src/app/components/DashBoardComponents/HeadMainPart.js
@@ -1,21 +1,23 @@
-
 import React from "react";
 import Link from "next/link";
 import PLUS from "../icons/plus-icon";
 import classes from "../ui/button.module.css";
 import styles from "./HeadMainPart.module.css";
-import { options } from "../../api/auth/[...nextauth]/option";
 import { getServerSession } from "next-auth/next";
-import RoomUpdate from "./RoomUpdate"
+import { options } from "../../api/auth/[...nextauth]/option";
 
-async function MainPartDashboard({disp}) {
-  const session = await getServerSession(options);
-  const user = session?.user;
+async function MainPartDashboard() {
+  let user = null;
+  try {
+    const session = await getServerSession();
+    user = session?.user;
+  } catch (error) {
+    console.error("Error while fetching session:", error);
+  }
 
-  
   return (
     <>
-      {session ? (
+      {user ? (
         <div className={styles.container}>
           <div className={styles.left}>
             <div className={styles.leftContainer}>
@@ -33,13 +35,11 @@ async function MainPartDashboard({disp}) {
               <p>11270</p>
               <p>Online</p>
             </div>
-            <div className={styles.createNewRoom} >
-              
-                <button className={classes.btn}>
-                  <PLUS />
-                  <span>Create</span>
-                </button>
-              
+            <div className={styles.createNewRoom}>
+              <button className={classes.btn}>
+                <PLUS />
+                <span>Create</span>
+              </button>
             </div>
           </div>
         </div>
